b2d02b6f61d0d57cfc57670a0aeafe293b7c5475
kop4anskiy <70712757+kop4anskiy@users.noreply.github.com>
2021-09-05 19:02:25 +0300

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace pr2_01_02
@@ -27,3 +27,79 @@ namespace pr2_01_02
         }
     }
 }
+/*
+* using System;
+using System.IO;
+
+namespace pr2_01_01
+{
+ class Program
+ {
+   static void ShowDirectory(DirectoryInfo dir)
+   {
+     // Показать все файлы
+     foreach (FileInfo file in dir.GetFiles())
+     {
+       Console.WriteLine("File: {0}", file.FullName);
+     }
+
+     foreach (DirectoryInfo subDir in dir.GetDirectories())
+     {
+       ShowDirectory(subDir);
+     }
+   }
+
+   static void Main(string[] args)
+   {
+     // C#
+     DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory); 
+     ShowDirectory(dir);
+   }
+ }
+}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace Compression
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           string inFilename = @"d:\ttt.txt";
+           string outFilename = @"d:\ttt.txt.gz";
+           //FileStream sourceFile = File.OpenRead(inFilename);
+           //FileStream destFile = File.Create(outFilename);
+
+           //GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+           //int theByte = sourceFile.ReadByte();
+           //while (theByte != -1)
+           //{
+           //    compStream.WriteByte((byte)theByte);
+           //    theByte = sourceFile.ReadByte();
+           //}
+           //compStream.Close();
+
+
+           string inFilename1 = @"d:\tttt.txt";
+           FileStream sourceFile = File.OpenRead(outFilename);
+           FileStream destFile = File.Create(inFilename1);
+           GZipStream compStream =
+           new GZipStream(sourceFile, CompressionMode.Decompress);
+           int theByte = compStream.ReadByte();
+           while (theByte != -1)
+           {
+               destFile.WriteByte((byte)theByte);
+               theByte = compStream.ReadByte();
+           }
+
+           compStream.Close();
+
+
+       }
+   }
+} */ 
